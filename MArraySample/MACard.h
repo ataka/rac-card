8ed3626edddcbba9b116499170faa0dba180db09
Masayuki Ataka <masayuki.ataka@gmail.com>
2014-07-23 08:42:29 +0900

    New class MACard

---
+++ MArraySample/MACard.h
@@ -0,0 +1,13 @@
+//
+//  MACard.h
+//  MArraySample
+//
+//  Created by 安宅 正之 on 2014/07/23.
+//  Copyright (c) 2014年 MasayukiAtaka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MACard : NSObject
+
+@end
---
+++ MArraySample/MACard.m
@@ -0,0 +1,13 @@
+//
+//  MACard.m
+//  MArraySample
+//
+//  Created by 安宅 正之 on 2014/07/23.
+//  Copyright (c) 2014年 MasayukiAtaka. All rights reserved.
+//
+
+#import "MACard.h"
+
+@implementation MACard
+
+@end
